52bee2c2c64854ce3efe005f278670ea22e03291
Sebastian Czech <sebaczech@gmail.com>
2022-05-26 23:32:30 +0200

    Create simple VNC using Terraform Cloud

--- main.tf
+++ main.tf
@@ -23,3 +23,10 @@ provider "oci" {
   region                = var.region
   config_file_profile   = var.profile_name
 }
+
+resource "oci_core_vcn" "internal" {
+  dns_label      = "internal"
+  cidr_block     = "172.16.0.0/20"
+  compartment_id = var.compartment_ocid
+  display_name   = "VNC TF Cloud"
+}
