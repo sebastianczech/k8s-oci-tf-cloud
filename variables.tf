b91fd465fbcd475ee5231339fda3d52e0cde7d97
Sebastian Czech <sebaczech@gmail.com>
2022-05-26 23:37:36 +0200

    Add variables

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "tenancy_ocid" {
+  type        = string
+}
+
+variable "compartment_ocid" {
+  type        = string
+}
+
+variable "user_ocid" {
+  type        = string
+}
+
+variable "private_key" {
+  type        = string
+}
+
+variable "fingerprint" {
+  type        = string
+}
+
+variable "region" {
+  type        = string
+}
+
+variable "profile_name" {
+  type        = string
+}
