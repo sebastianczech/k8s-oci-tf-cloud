ff37d7a342dd5f74b22d1bc83e520019bf7f60ea
Sebastian Czech <sebaczech@gmail.com>
2022-09-15 20:01:04 +0200

    Use validation for ingress and egrees rules

--- variables.tf
+++ variables.tf
@@ -37,9 +37,41 @@ variable "region" {
 variable "egress_security_rules" {
   type    = list(map(string))
   default = []
+  validation {
+    condition = (length(var.egress_security_rules) > 0 && alltrue([for rule in var.egress_security_rules : can(rule["protocol"])]))
+    error_message = "Every item in the egress security rules has to contain procotol"
+  }
+  validation {
+    condition = (length(var.egress_security_rules) > 0 && alltrue([for rule in var.egress_security_rules : can(rule["destination"])]))
+    error_message = "Every item in the egress security rules has to contain destination"
+  }
+  validation {
+    condition = (length(var.egress_security_rules) > 0 && alltrue([for rule in var.egress_security_rules : can(rule["destination_type"])]))
+    error_message = "Every item in the egress security rules has to contain destination_type"
+  }
+  validation {
+    condition = (length(var.egress_security_rules) > 0 && alltrue([for rule in var.egress_security_rules : can(rule["description"])]))
+    error_message = "Every item in the egress security rules has to contain description"
+  }
 }
 
 variable "ingress_security_rules" {
   type    = list(map(string))
   default = []
+  validation {
+    condition = (length(var.ingress_security_rules) > 0 && alltrue([for rule in var.ingress_security_rules : can(rule["protocol"])]))
+    error_message = "Every item in the ingress security rules has to contain procotol"
+  }
+  validation {
+    condition = (length(var.ingress_security_rules) > 0 && alltrue([for rule in var.ingress_security_rules : can(rule["source"])]))
+    error_message = "Every item in the ingress security rules has to contain source"
+  }
+  validation {
+    condition = (length(var.ingress_security_rules) > 0 && alltrue([for rule in var.ingress_security_rules : can(rule["source_type"])]))
+    error_message = "Every item in the ingress security rules has to contain source_type"
+  }
+  validation {
+    condition = (length(var.ingress_security_rules) > 0 && alltrue([for rule in var.ingress_security_rules : can(rule["description"])]))
+    error_message = "Every item in the ingress security rules has to contain description"
+  }
 }
